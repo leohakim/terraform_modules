8a724ff2ed75088ea1b5514f479f9cb287a20948
Leohakim <leohakim@gmail.com>
2021-12-30 15:44:17 +0100

    Terraform modules

---
+++ instance/variables.tf
@@ -0,0 +1,20 @@
+variable "ami_id" {
+  default     = ""
+  description = "Ami Id"
+}
+
+variable "instance_type" {
+
+}
+
+variable "tags" {
+  type = map(any)
+}
+
+variable "sg_name" {
+
+}
+
+variable "ingress_rules"{
+  
+}
